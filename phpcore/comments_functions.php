240e6e5f5fbb5fa7b19edfb7bd0f3c801dbf00ec
Дмитрий Демьянюк <dmitrd@rarus.ru>
2013-11-21 22:47:20 +0400

    Initial commit

---
+++ phpcore/comments_functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Diamond
+ * Date: 09.08.13
+ * Time: 13:33
+ * To change this template use File | Settings | File Templates.
+ */
+
+function GetProjCommentList($idProj, $idTask = false, $idStage = false)
+{//project commentaries
+
+}
